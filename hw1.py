fdad2249c0321476678f3a82d7ed60d14d383047
GGGbot <agusha443@gmail.com>
2018-02-19 15:58:36 +0300

    123

---
+++ hw1.py
@@ -0,0 +1,11 @@
+Python 3.7.0b1 (v3.7.0b1:9561d7f501, Jan 30 2018, 19:10:11) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> print("Hello world!")
+Hello world!
+>>> 
